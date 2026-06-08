be430a2bf408a6526378544c34dd06b52bd84465
DemuraAIdev <cyberaioff@gmail.com>
2026-06-08 18:18:30 +0700

    HINT

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import "./altair.css";
 import App from "./App.vue";
 
+console.log("hello");
+console.log("Are you human ?");
+console.log("I will find you !");
+console.log("Try to rub the page");
+
 createApp(App).mount("#app");
